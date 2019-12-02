04eb11c5a05087dddaa2f16b73e86221b1b60ea2
Guillaume Libersat <glibersat@sigill.org>
2019-12-02 15:14:50 +0100

    move erpnext_client to its own package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ERPNext Client',
+      version='0.1',
+      description='ERPnext REST API Client',
+      author='Guillaume Libersat',
+      author_email='glibersat@singe-savant.com',
+      url='https://github.com/singesavant/erpnext-rest-client',
+      packages=['erpnext_client'],
+     )
